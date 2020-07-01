86ec05c558bae84f88a115950d30862dc6db377a
aduii <alexjuep@hotmail.com>
2020-07-01 02:50:31 -0500

    Changes to deploy in heroku

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ const path = require('path');
  
 const app = express();
  
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/bonds-front'));
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/bonds-front'));
  
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/bonds-front/index.html'));
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/bonds-front/index.html'));
 });
- 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
